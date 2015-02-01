d426712d2c1d6eb0a5958009177e9aebb26da2ca
ludenviser <d.ljungqvist@gmail.com>
2015-02-01 16:54:41 +0100

    Removed included files

---
+++ lab1/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#ifndef STDAFX_H
+
+#include "targetver.h"
+#include <iostream>
+
+
+#endif // !STDAFX_H
+
+
+
+
+
+// TODO: reference additional headers your program requires here
